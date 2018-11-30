8f3e0bb580e0522d274a65b66ee17f3c58b75421
dqszxdbcyezfckq <45467604+dqszxdbcyezfckq@users.noreply.github.com>
2018-11-30 17:38:34 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -9,7 +9,7 @@ module.exports = client => { // eslint-disable-line no-unused-vars
  //client.user.setGame('//help pour plus d\'info', 'https://www.twitch.tv/ryvalgaming')//stream
   //client.user.setAvatar("https://cdn.discordapp.com/attachments/515913352430747665/515917419198545930/rainbow.gif");//avatar
 
-  client.user.setActivity(`${client.users.get('281125214098685954').username}#5682`, {type: "WATCHING"});
+  client.user.setActivity(`//help pour recevoir les commandes`);
   
 
 };
